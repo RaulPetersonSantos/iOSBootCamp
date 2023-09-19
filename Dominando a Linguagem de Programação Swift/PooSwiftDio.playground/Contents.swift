70e0d9bca4a3efbeef92d104eff24e9d7325c595
raul.santos <raul.santos@valemobi.com.br>
2023-09-19 10:06:46 -0300

    Feat: desafio POO

---
+++ Dominando a Linguagem de Programação Swift/PooSwiftDio.playground/Contents.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+enum PayMethod {
+    case cash
+    case credit
+    case debit
+    
+    var description: String {
+          switch self {
+          case .credit:
+              return "Crédito"
+          case .debit:
+              return "Débito"
+          case .cash:
+              return "Dinheiro"
+          }
+      }
+}
+
+class Product {
+    
+    private var name: String = ""
+    private var price: Double = 0.0
+    private var category: String = ""
+
+    init(name: String, price: Double, category: String) {
+        self.name = name
+        self.price = price
+        self.category = category
+    }
+    
+    func buyProduct(withPay: PayMethod, finance: Int? = nil)  {
+        
+        var message = """
+                   Obrigado por realizar a compra, segue a nota fiscal
+                   =================
+                   Produto: \(name)
+                   Preço: \(String(format: "%.2f", price))
+                   Categoria: \(category)
+                   Forma de Pagamento: \(withPay.description)
+                   """
+
+               if withPay == .credit, let finance = finance {
+                   message += "\nParcelamento: \(finance)"
+               }
+
+               message += "\n================="
+
+               print(message)
+    }
+    
+  
+    
+}
+
+let Apple = Product(name: "Apple", price: 5.00, category: "Food")
+let playStation = Product(name: "PS5", price: 3.800, category: "Eletronic")
+
+playStation.buyProduct(withPay: .credit, finance: 2)
+Apple.buyProduct(withPay: .cash)
